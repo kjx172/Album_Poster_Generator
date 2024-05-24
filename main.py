0ecd8b40b722bf513a52421a270db684c98ffe50
Aaron Kisitu <128210948+kjx172@users.noreply.github.com>
2024-05-24 16:42:25 -0500

    add album name and artist name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from album_retrival import get_releasegroupid, get_releaseID_from_releasegroup, get_cover, get_tracklist, get_palette, get_duration, get_label_info, get_release_date # type: ignore
+from album_retrival import get_releasegroupid, get_releaseID_from_releasegroup, get_cover, get_tracklist, get_palette, get_duration, get_label_info, get_release_date, get_album_name, get_artist_name # type: ignore
 from poster_creator import create_poster #type: ignore
 
 # Defining main function 
@@ -11,16 +11,19 @@ def main():
     if album_releasegroupid:
         album_id = get_releaseID_from_releasegroup(album_releasegroupid)
 
-    #gets the the album cover, tracklist, palette, tracklist, length, label info, release date. 
+    #gets the the album cover, tracklist, palette, tracklist, length, label info, release date, album_name. 
     album_cover = get_cover(album_id)
     tracklist = get_tracklist(album_id)
     palette = get_palette(album_cover)
     album_length = get_duration(album_id)
     label_info = get_label_info(album_id)
     release_date = get_release_date(album_id)
+    album_name = get_album_name(album_id)
+    artist_name = get_artist_name(album_releasegroupid)
+
 
     #passes album cover into poster creator
-    create_poster(album_cover, search_album, tracklist, palette, album_length, label_info, release_date)
+    create_poster(album_cover, tracklist, palette, album_length, label_info, release_date, album_name, artist_name)
   
   
 # Using the special variable __name__ 
