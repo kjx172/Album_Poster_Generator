0759014c0d462f7333909f057498d47dd5f9e42a
Aaron Kisitu <128210948+kjx172@users.noreply.github.com>
2024-05-24 14:59:29 -0500

    added info
    gets album length, labels, and release date

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from album_retrival import get_releasegroupid, get_releaseID_from_releasegroup, get_cover, get_tracklist, get_palette # type: ignore
+from album_retrival import get_releasegroupid, get_releaseID_from_releasegroup, get_cover, get_tracklist, get_palette, get_duration, get_label_info, get_release_date # type: ignore
 from poster_creator import create_poster #type: ignore
 
 # Defining main function 
@@ -11,13 +11,16 @@ def main():
     if album_releasegroupid:
         album_id = get_releaseID_from_releasegroup(album_releasegroupid)
 
-    #gets the the album cover, tracklist, palette
+    #gets the the album cover, tracklist, palette, tracklist, length, label info, release date. 
     album_cover = get_cover(album_id)
     tracklist = get_tracklist(album_id)
     palette = get_palette(album_cover)
+    album_length = get_duration(album_id)
+    label_info = get_label_info(album_id)
+    release_date = get_release_date(album_id)
 
     #passes album cover into poster creator
-    create_poster(album_cover, search_album, tracklist, palette)
+    create_poster(album_cover, search_album, tracklist, palette, album_length, label_info, release_date)
   
   
 # Using the special variable __name__ 
